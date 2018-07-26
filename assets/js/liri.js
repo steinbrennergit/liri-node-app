58c14b2774892b045358d4219498110cb25fe88b
steinbrennergit <steinbrennergit@gmail.com>
2018-07-26 13:56:26 -0700

    refactored

--- assets/js/liri.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require("dotenv").config();
-const req = require("request");
-const Twitter = require("twitter");
-const Spotify = require("spotify");
-const keys = require("./keys.js");
-
-var client = new Twitter(keys.twitter);
-
